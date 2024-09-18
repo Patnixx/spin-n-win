b52fb28403e32eaef76c036c2ba81962012010c4
patrik <patkonemcok@gmail.com>
2024-09-18 09:13:06 +0200

    Migrations & Models init

--- database/migrations/2024_09_18_070729_create_games_table.php
+++ database/migrations/2024_09_18_070729_create_games_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('age');
-            $table->rememberToken();
+            $table->string('type');
+            $table->string('picture_path')->unique();
+            $table->integer('low_price');
+            $table->integer('high_price');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('games');
     }
 };
