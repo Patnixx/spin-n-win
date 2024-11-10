2f026b1d25e61164ed20126291ef60b5f9104ec5
fabvoj <fvojar@gmail.com>
2024-11-10 11:38:41 +0100

    danger text uz nie je dojebany, neskor jak to este dorobim responzivitu

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,24 +8,23 @@
       <!-- NOTE - Username -->
       <div class="input-container">
         <input class="input-placeholder" type="username" id="username" name="username" placeholder="Username" class="@error('username') is-invalid @enderror" value="{{@old('username')}}">
+        @if ($errors->has('password'))
+          <span class="text-danger" role="alert">{{$errors->first('password')}}</span>
+        @endif
       </div>
 
       <!-- NOTE - Password -->
       <div class="input-container">
         <input  class="input-placeholder" type="password" id="password" name="password" placeholder="Password" class="@error('password') is-invalid @enderror" value="{{@old('password')}}" minlength="6">
-        @if ($errors->has('password'))
-          <span class="text-danger" role="alert"><strong>{{$errors->first('password')}}</strong></span>
-        @endif
         @if ($errors->has('username'))
-          <span class="text-danger" role="alert"><strong>{{$errors->first('username')}}</strong></span>
+          <span class="text-danger" role="alert">{{$errors->first('username')}}</span>
         @endif
-        
       </div>
         <button class="login-btn" type="submit">Log In</button>
         <div class="sub-btns" >
-        <a class="sub-btn" href="{{route('register')}}">Register</a>
-      <a class="sub-btn" href="#">Forgot your password?</a>
-      </div
+         <a class="sub-btn" href="{{route('register')}}">Register</a>
+         <a class="sub-btn" href="#">Forgot your password?</a>
+        </div>
     </form>
   </div>
 </body>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,7 +9,7 @@
       <div class="input-container">
         <input class="input-placeholder" type="text" id="f_name" name="f_name" placeholder="First Name" class="@error('f_name') is-invalid @enderror" value="{{@old('f_name')}}">
         @if ($errors->has('f_name'))
-          <span class="text-danger" role="alert"><strong>{{$errors->first('f_name')}}</strong></span>
+          <span class="text-danger" role="alert">{{$errors->first('f_name')}}</span>
         @endif
       </div>
 
@@ -17,7 +17,7 @@
       <div class="input-container">
         <input class="input-placeholder" type="text" id="l_name" name="l_name" placeholder="Last Name" class="@error('l_name') is-invalid @enderror" value="{{@old('l_name')}}">
         @if ($errors->has('l_name'))
-          <span class="text-danger" role="alert"><strong>{{$errors->first('l_name')}}</strong></span>
+          <span class="text-danger" role="alert">{{$errors->first('l_name')}}</span>
         @endif
       </div>
       
@@ -25,7 +25,7 @@
       <div class="input-container">
         <input class="input-placeholder" type="text" id="username" name="username" placeholder="Username" class="@error('username') is-invalid @enderror" value="{{@old('username')}}">
         @if ($errors->has('username'))
-          <span class="text-danger" role="alert"><strong>{{$errors->first('username')}}</strong></span>
+          <span class="text-danger" role="alert">{{$errors->first('username')}}</span>
         @endif
       </div>
 
@@ -33,7 +33,7 @@
       <div class="input-container">
         <input class="input-placeholder" type="email" id="email" name="email" placeholder="Email" class="@error('email') is-invalid @enderror" value="{{@old('email')}}">
         @if ($errors->has('email'))
-          <span class="text-danger" role="alert"><strong>{{$errors->first('email')}}</strong></span>
+          <span class="text-danger" role="alert">{{$errors->first('email')}}</span>
         @endif
       </div>
 
@@ -41,7 +41,7 @@
       <div class="input-container">
         <input class="input-placeholder" type="password" id="password" name="password" placeholder="Password" class="@error('password') is-invalid @enderror" value="{{old('pass')}}">
         @if ($errors->has('password'))
-          <span class="text-danger" role="alert"><strong>{{$errors->first('password')}}</strong></span>
+          <span class="text-danger" role="alert">{{$errors->first('password')}}</span>
         @endif
       </div>
 
@@ -49,12 +49,7 @@
       <div class="input-container">
         <input class="input-placeholder" type="password" id="cpassword" name="cpassword" placeholder="Confirm Password" class="@error('cpassword') is-invalid @enderror" autocomplete="new-password">
         @if ($errors->has('cpassword'))
-          <span class="text-danger" role="alert"><strong>{{$errors->first('cpassword')}}</strong></span>
-        @endif
-        @if ($errors->any())
-          @foreach ($errors->all() as $error)
-            <span class="text-danger" role="alert"><strong>{{$error}}</strong></span>
-          @endforeach
+          <span class="text-danger" role="alert">{{$errors->first('cpassword')}}</span>
         @endif
       </div>
         <button class="register-btn" type="submit">Register</button>
